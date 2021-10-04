060b7c025ce57b2cdd0775776253fe8d7e3cf837
Violet <nechaevaviolet@gmail.com>
2021-10-04 13:39:25 +0300

    rename

--- HW01/Block3/run.sh
+++ HW01/Block3/run.sh
@@ -5,9 +5,9 @@ HADOOP_STREAMING_JAR=/opt/hadoop-3.2.1/share/hadoop/tools/lib/hadoop-streaming-3
 hdfs dfs -rm -r /HW01/output
 
 yarn jar $HADOOP_STREAMING_JAR \
-	-files mean_variance_mapper.py, mean_variance_reducer.py \
-	-mapper "python3 ./mean_variance_mapper.py" \
-	-reducer "python3 ./mean_variance_reducer.py" \
+	-files mapper.py, reducer.py \
+	-mapper "python3 ./mapper.py" \
+	-reducer "python3 ./reducer.py" \
 	-numReduceTasks 1 \
 	-input /AB_NYC_2019.csv \
 	-output /output
