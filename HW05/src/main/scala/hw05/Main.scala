54830d0cdb1f3855b8128c4d3b94f3fd8350b104
user-vi <50022091+user-vi@users.noreply.github.com>
2021-12-05 13:10:05 +0300

    hw05

--- HW05/src/main/scala/hw05/Main.scala
+++ HW05/src/main/scala/hw05/Main.scala
@@ -1,7 +1,8 @@
 package hw05
 
-import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.linalg.{DenseMatrix, DenseVector, sum}
 import breeze.numerics.abs
+import breeze.optimize.{DiffFunction, LBFGS}
 import breeze.stats.mean
 
 import scala.annotation.tailrec
@@ -9,11 +10,11 @@ import scala.annotation.tailrec
 
 object Main extends App {
 
-  def mape(y: DenseVector[Double], y_hat: DenseVector[Double]): Double = {
-    mean(abs((y - y_hat) / y))
+  def mae(y: DenseVector[Double], y_hat: DenseVector[Double]): Double = {
+    mean(abs(y - y_hat))
   }
 
-  val model = new SGD(epochs = 1000, learningRate = 0.001)
+  val model = new SGD()
 
   val X_train: DenseMatrix[Double] = DenseMatrix.rand[Double](1000000, 3)
   val y_train: DenseVector[Double] = X_train * DenseVector[Double](1.5, 0.3, -0.7)
@@ -24,12 +25,12 @@ object Main extends App {
   model.fit(X_train, y_train)
   val y_hat: DenseVector[Double] = model.predict(X_test)
 
-  val score = mape(y_test, y_hat)
-  println("mape = " + score)
+  val score = mae(y_test, y_hat)
+  println(model.cofficients())
 }
 
 
-class SGD(val epochs: Int, val learningRate: Double) {
+class SGD() {
   var w: DenseVector[Double] = _
 
   def fit(X: DenseMatrix[Double], y: DenseVector[Double]): Unit = {
@@ -37,33 +38,26 @@ class SGD(val epochs: Int, val learningRate: Double) {
     val X_ = DenseMatrix.horzcat(ones, X)
     w = DenseVector.rand[Double](X_.cols)
 
-    def updateWeight(row: Int, w: DenseVector[Double]): DenseVector[Double] = {
-      val grad = X_(row, ::) * (X_(row, ::) * w - y(row))
-      w - this.learningRate * grad.t
+    val optimizationPipeline = new DiffFunction[DenseVector[Double]] {
+      def calculate(w: DenseVector[Double]): (Double, DenseVector[Double]) = {
+        val e = X_ * w - y
+        val loss = sum(e ^:^ 2.0) / (2 * X_.rows)
+        val grad = e.t * X_ /:/ (2.0 * X_.rows)
+        (loss, grad.t)
+      }
     }
-
-    @tailrec
-    def loop(increment: Int,
-             f: (Int, DenseVector[Double]) => DenseVector[Double],
-             w: DenseVector[Double],
-            ): DenseVector[Double] = {
-      if (increment == 1) w
-      else loop(increment - 1, updateWeight, updateWeight(increment - 1, w))
-    }
-
-    @tailrec
-    def train(epoch: Int): DenseVector[Double] = {
-      if (epoch == 1) loop(increment = X_.rows, updateWeight, w)
-      else train(epoch - 1)
-    }
-
-    train(this.epochs)
+    val optimizer = new LBFGS[DenseVector[Double]]()
+    w = optimizer.minimize(optimizationPipeline, DenseVector(0.0, 0.0, 0.0, 0.0))
   }
 
   def predict(X: DenseMatrix[Double]): DenseVector[Double] = {
     val ones = DenseMatrix.fill[Double](X.rows, 1)(1)
     val X_ = DenseMatrix.horzcat(ones, X)
     X_ * w
+  }
+
+  def cofficients(): DenseVector[Double] = {
+    w(1 to 3)
   }
 }
 
